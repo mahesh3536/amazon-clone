4ec6da446148ccd9425874ed89fb426cfdb498fa
mahesh3536 <maheshjivarani3536@gmail.com>
2023-01-24 17:17:44 +0530

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,75 @@
+import Image from "next/image";
+import React from "react";
+import {
+    Bars3Icon,
+  MagnifyingGlassIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/24/outline";
+function Header() {
+  return (
+    <header>
+      <div className="flex items-center bg-amazon_blue p-1 py-2 flex-grow">
+        <div className="flex flex-grow items-center mt-2 sm:flex-grow-0 mr-2">
+          <Image
+            src="https://links.papareact.com/f90"
+            alt="amazon"
+            width={150}
+            height={40}
+            className="object-contain w-auto  cursor-pointer"
+          />
+        </div>
+        {/* {searchbar} */}
+        <div className=" hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer  bg-yellow-400 hover:bg-yellow-500">
+          <input
+            type="text"
+            className="h-full p-2 flex-grow w-6 rounded-md flex-shrink focus:outline-none"
+          />
+          <MagnifyingGlassIcon className="h-12 p-4" />
+        </div>
+        {/* right */}
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div className=" link ">
+            <p>Hello Mahesh</p>
+            <p className="font-extrabold md:text-sm">Account & list</p>
+          </div>
+          <div className=" link ">
+            <p>Returns</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
+          </div>
+          <div className="relative link flex items-center">
+            <span className="absolute top-0 right-0 md:right-10 bg-yellow-400 text-center h-4 w-4 rounded-full text-black font-bold">
+              4
+            </span>
+            <ShoppingCartIcon className="h-10 " />
+            <p className="hidden md:inline font-extrabold md:text-sm mt-2">
+              Basket
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Bottom view */}
+      <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+        <p className="link flex items-center ">
+        <Bars3Icon className="h-6 mr-1"/>
+        All
+        </p>
+        <p className="link">
+            Prime video
+        </p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today's deals</p>
+        <p className="link hidden lg:inline">Electronics</p>
+        <p className="link hidden lg:inline">Food & grocery</p>
+        <p className="link hidden lg:inline">Prime</p>
+        <p className="link hidden lg:inline">Buy again</p>
+        <p className="link hidden lg:inline">Shopper toolkit</p>
+        <p className="link hidden lg:inline">Health & personal care</p>
+      </div>
+
+
+    </header>
+  );
+}
+
+export default Header;
---
+++ components/Product.tsx
@@ -0,0 +1,40 @@
+import { StarIcon } from '@heroicons/react/24/solid'
+import React, { useState } from 'react'
+const MAX_RATING = 5;
+const MIN_RATING = 1;
+export default function Product({id,title,price,description,category,image}:any) {
+  // const [rating] = useState(
+  //   Math.floor(Math.random() + (MAX_RATING-MIN_RATING+1)) + MIN_RATING
+  // );
+  // const [hasPrime] = useState(Math.random() < 0.5);
+  return (
+    <div className='relative flex flex-col m-5 bg-red z-30 p-10 justify-center bg-white'>
+        <p className='absolute top-2 right-2 text-xs italic text-gray-400'>{category}</p>
+        <div className='flex flex-col justify-center items-center'>
+        <img src={image} height={200} width={200} className="object-contain"/>
+        </div>
+        
+        <h4>{title}</h4>
+        <div className='flex'>
+            {
+                Array(4).fill(1).map((_,i)=>(
+                    <StarIcon className='h-5 text-yellow-500'/>
+                ))
+            }
+        </div>
+        <p className='text-xs my-2 line-clamp-2'>{description}</p>
+        <div className='mb-5'>
+          {price}
+        </div>
+        {
+          true && (
+            <div className='flex items-center space-x-2 -mt-5'>
+              <img src="https://links.papareact.com/fdw" alt="" className='w-12'/>
+              <p className='text-xs text-gray-500'>Free next day delivery</p>
+            </div>
+          )
+        }
+        <button className='mt-auto button'>Add to basket</button>
+    </div>
+  )
+}
